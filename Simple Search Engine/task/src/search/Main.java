201f4f5d8ffcce41936efeba7342a78c47b74c5b
Indytskyi <ind.artem2003@gmail.com>
2022-06-17 15:05:04 +0300

    first module

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,6 +1,21 @@
 package search;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void findWord() {
+        Scanner scanner = new Scanner(System.in);
+        String stringInput = scanner.nextLine();
+        String[] stringArray = stringInput.split(" ");
+        String currentWord = scanner.nextLine();
+        for (int i = 0; i < stringArray.length; i++) {
+            if (stringArray[i].equals(currentWord)) {
+                System.out.println(i + 1);
+                return;
+            }
+        }
+        System.out.println("Not found");
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
